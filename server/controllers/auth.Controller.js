1341c7f0ec0ea8c1da7de7100cdc91d2736d6b31
Rajan Kumar <rk4009023@gmail.com>
2025-08-21 01:39:33 +0530

    update sum file

--- server/controllers/auth.Controller.js
+++ server/controllers/auth.Controller.js
@@ -38,8 +38,8 @@ export const register = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      secure: true,        // Render/Vercel => true
+      sameSite: "none",    // cross-site requests allow
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
 
@@ -79,8 +79,8 @@ export const login = async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      secure: true,        // Render/Vercel => true
+      sameSite: "none",    // cross-site requests allow
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
 
@@ -95,8 +95,8 @@ export const logout = async (req, res) => {
   try {
     res.clearCookie('token', {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      secure: true,        // Render/Vercel => true
+      sameSite: "none",    // cross-site requests allow
     });
     return res.json({ success: true, message: "Logged Out" });
   } catch (error) {
